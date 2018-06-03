d6f1d0f22b0f6598d6f5f2b0d42d7ed297c9509c
Luis Thomas <thomasluis555@gmail.com>
2018-06-03 14:42:33 -0400

    using redux, a new post can be added to the list

--- src/components/PostForm.js
+++ src/components/PostForm.js
@@ -1,6 +1,9 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { createPost } from '../actions/postActions';
 
-export default class Postform extends Component {
+class Postform extends Component {
     constructor(props) {
         super(props);
         this.state = {
@@ -21,15 +24,8 @@ export default class Postform extends Component {
             title: this.state.title,
             body: this.state.body
         }
-        fetch('https://jsonplaceholder.typicode.com/posts', {
-            method: 'POST',
-            headers: {
-                'content-type': 'application/json'
-            },
-            body: JSON.stringify(post)
-        })
-        .then(res => res.json())
-        .then(data => console.log(data));
+
+        this.props.createPost(post);
     }
     render() {
     return (
@@ -52,3 +48,9 @@ export default class Postform extends Component {
     )
   }
 }
+
+Postform.propTypes = {
+    createPost: PropTypes.func.isRequired
+};
+
+export default connect(null, { createPost })(Postform);
--- src/reducers/postReducer.js
+++ src/reducers/postReducer.js
@@ -7,7 +7,9 @@ const initialState = {
 export default function(state = initialState, action) {
     switch(action.type) {
         case FETCH_POSTS:
-            return {...state, items: action.payload}
+            return {...state, items: action.payload};
+        case NEW_POST: 
+        return {...state, item: action.payload};
         default: 
             return state;
     }
