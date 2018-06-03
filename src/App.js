4ebcfb6bff0d45cbdcd311961b10d1c344c37b9e
Luis Thomas <thomasluis555@gmail.com>
2018-06-03 14:11:56 -0400

    fetch get method to json api is now being done using redux.

--- src/App.js
+++ src/App.js
@@ -2,12 +2,11 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { Provider } from'react-redux';
-import { createStore, applyMiddleware } from 'redux';
 
 import Posts from './components/Posts';
 import PostForm from './components/PostForm';
+import store from './store';
 
-const store = createStore(() => [], {}, applyMiddleware());
 
 class App extends Component {
   render() {
